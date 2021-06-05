841348db942b0f121540a87cec9180d20123feb9
Keyur R. Machhi <Y05477@ad.csci.co.in>
2021-06-05 17:27:13 +0530

    interceptor and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,38 @@ import { SigninComponent } from './pages/signin/signin.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
+
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+const redirectUnauthorizedToSignUp = () => redirectUnauthorizedTo(['signin']);
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'signin', component: SigninComponent },
+  {
+    path: 'signin',
+    component: SigninComponent,
+    ...canActivate(redirectLoggedInToHome),
+  },
   { path: 'signup', component: SignupComponent },
-  { path: 'user/:userName', component: UserDetailComponent },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'user/:userName',
+    component: UserDetailComponent,
+    ...canActivate(redirectUnauthorizedToSignUp),
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    ...canActivate(redirectUnauthorizedToSignUp),
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,15 +1,38 @@
+import { ToastrService } from 'ngx-toastr';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  constructor(
+    private _authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastr: ToastrService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(f: NgForm) {
+    // console.log(f.value);
+    const { email, password } = f.value;
+    if (f.valid) {
+      this._authService
+        .signUp(email, password)
+        .then((res: any) => {
+          this.router.navigate(['/'], { relativeTo: this.route });
+          this.toastr.success('Register success !!!');
+        })
+        .catch((err: any) => {
+          console.log(err);
+          this.toastr.error(err.message, 'Something went wrong');
+        });
+    }
   }
-
 }
