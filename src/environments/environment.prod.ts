9bf9cf8eb1265957b6df9fe49b8a423f4dd423c2
Keyur Machhi <keyurmachhi123@gmail.com>
2021-06-21 21:00:45 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyBsVM_zYrQ0KbAhOk_kQFv1-vWVcCe-C7g',
-    authDomain: 'github-searcher-a7a4f.firebaseapp.com',
-    projectId: 'github-searcher-a7a4f',
-    storageBucket: 'github-searcher-a7a4f.appspot.com',
-    messagingSenderId: '698579496490',
-    appId: '1:698579496490:web:33c99aba9eee6eb8149d53',
-    measurementId: 'G-H3404PLYE7',
-  },
-  GITHUB_CLIENT_ID: 'c0c0dc9fbd0351584b9e',
-  GITHUB_CLIENT_SECRETE_KEY: 'fcfd6d5b5bae741a2a5c99d6c4c4729d4183214e',
 };
